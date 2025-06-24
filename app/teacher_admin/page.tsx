a11459df6d394a83178848a3a83feccac217c4db
Paul-Peter Arslan <paulpeterarslan@gmail.com>
2025-06-24 13:58:28 +0200

    security for teacher interface and apostrophe

--- app/teacher_admin/page.tsx
+++ app/teacher_admin/page.tsx
@@ -57,7 +57,7 @@ export default function TeacherListPage() {
       <h4 style={{ fontSize: "1rem", marginBottom: "0.5rem" }}>Add a song</h4>
       <div style={{ marginBottom: "2rem" }}>
         <label style={{ fontSize: "0.9rem" }}>
-          Song title:{" "}
+          Song Title:{" "}
           <input
             type="text"
             value={title}
